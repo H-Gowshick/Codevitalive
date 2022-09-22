b5676411997e265663457b4a0927010b43abb175
H-Gowshick <107055849+H-Gowshick@users.noreply.github.com>
2022-09-22 11:24:21 +0530

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -128,6 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
- 
-STATIC_ROOT=[
+STATICFILES_DIRS = [
     BASE_DIR / "static"]
+STATIC_ROOT = "skill-brewery-intern.herokuapp.com/static/"
